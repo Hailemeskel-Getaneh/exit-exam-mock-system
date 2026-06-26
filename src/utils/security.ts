a090a0ea27255df7d5d76c84b09317880d3efabc
Hailemeskel-Getaneh <hailemeskel.getaneh@github.com>
2026-06-26 10:44:32 +0300

    fix: correct hashPassword to be deterministic (remove Math.random salt)

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -15,7 +15,7 @@ const HASH_ALGORITHM = 'SHA-256';
  */
 export const hashPassword = async (password: string): Promise<string> => {
   const encoder = new TextEncoder();
-  const data = encoder.encode(password + Math.random().toString());
+  const data = encoder.encode(password);
   const hashBuffer = await crypto.subtle.digest(HASH_ALGORITHM, data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
